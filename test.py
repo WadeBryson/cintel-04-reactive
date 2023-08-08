5f5fb9405ed60287faa34f374b1fcaa111686b8b
Wade Bryson <wade.j.bryson@gmail.com>
2023-08-08 14:03:54 -0500

    Deleted a bunch of unused data in nuclear.csv

--- test.py
+++ test.py
@@ -7,39 +7,10 @@ import plotly.express as px
 
 from util_logger import setup_logger
 
-p = pathlib.Path(__file__).parent.joinpath("data").joinpath("nuclear_explosions.csv")
+p = pathlib.Path(__file__).parent.joinpath("data").joinpath("nuclear.csv")
 # logger.info(f"Reading data from {p}")
-df = pd.read_csv(p)
-results = df.head(10)
-# print(results)
+original_df = pd.read_csv(p)
+total_count = len(original_df)
 
-# for col in df.columns:
-    # print(col)
-
-new_df = df[["WEAPON SOURCE COUNTRY"]]
-results2 = new_df.head(10)
-# print(results2)
-
-show_countries_list = []
-show_countries_list.append("FRANCE")
-show_countries_list.append("CHINA")
-
-country_filter = df["WEAPON SOURCE COUNTRY"].isin(show_countries_list)
-df3 = df[country_filter]
-results3 = df3.head(10)
-# print(results3)
-
-plotly_plot = px.scatter(
-            df,
-            x="Date.Year",
-            y="Data.Yeild.Upper",
-            color="WEAPON SOURCE COUNTRY",
-            title="Nuclear Explosions Chart",
-            labels={
-                "Date.Year": "Year",
-                "Data.Yeild.Upper": "Kilotons of TNT",
-            },
-            #TODO Bigger?
-            size_max=8,
-        )
-print(plotly_plot)
+results = original_df.head(10)
+print(results)
